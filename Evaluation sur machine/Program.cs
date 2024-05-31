be939d1c2aa1937e8a54f60fdc11f9e52bbc750c
Pigeon <pigeonevan6@yahoo.com>
2024-05-31 14:47:33 +0200

    Ajoutez des fichiers projet.

---
+++ Evaluation sur machine/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+double nbHarmoniques(double a)
+{
+    double res = 0.0;
+    
+
+    for (int i = 1; i <= a; i++)
+    {
+        res = 1.0 / i;
+    }
+
+    return res;
+}
+
+
+
+double n;
+Console.WriteLine("Saisir un entier: ");
+n = Convert.ToDouble(Console.ReadLine());
+
+double nbHarmonique = nbHarmoniques(n) ;
+Console.WriteLine($"Le nombre harmonique H({n}) est : {nbHarmoniques}");
